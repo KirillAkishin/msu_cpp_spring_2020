2b923105a53ed28c8638ccc01490c48d9fd5f097
CyrilAk <akishin.ka@yandex.ru>
2020-03-19 00:10:04 +0300

    minor fixes

--- 02/test.cpp
+++ 02/test.cpp
@@ -4,7 +4,7 @@
 
 int testCases(char * name){
     int testNumber = 0;
-    const char * text = "TEST STRING: 1231 adsd 22wd  4\t 12d31231231231233!";
+    const char * text = "TEST STRING: 1231 adsd 22wd  4\t 52d31231231231233!";
 
     //T1 - standart work
     testNumber++;
@@ -16,34 +16,29 @@ int testCases(char * name){
     parse(text);
     if (TEST_FILE != testNumber)
         return testNumber;
+    register_on_begin_callback([](){});
 
-    //T3 - standart work for register_on_end_callback 
+    //T3 - standart work for register_on_end_callback()
     testNumber++;
     register_on_end_callback([](){TEST_FILE = 3;});
     parse(text);
     if (TEST_FILE != testNumber)
         return testNumber;
+    register_on_end_callback([](){});
 
-    //T4 - standart work for register_on_number_callback 
+    //T4 - standart work for register_on_number_callback()
     testNumber++;
     register_on_number_callback([](const char * number){TEST_FILE = number[0];});
     parse(text);
-    if (TEST_FILE != 4)
+    if (TEST_FILE != '4')
         return testNumber;
 
-    //T5 - standart work for register_on_number_callback 
+    //T5 - standart work for register_on_string_callback()
     testNumber++;
-    register_on_number_callback([](const char * number){TEST_FILE = number[0];});
+    register_on_string_callback([](const char * str){TEST_FILE = str[0];});
     parse(text);
-    if (TEST_FILE != 4)
+    if (TEST_FILE != '5')
         return testNumber;
-
-        // register_on_end_callback([](){std::cout << "Testing onBegin()" << std::endl;});
-    // register_on_number_callback([](const char* number){ std::cout << "Testing onNumber() for: " 
-        // << number << std::endl;});
-    // register_on_string_callback([](const char* str){ std::cout << "Testing onString() for: " 
-        // << str << std::endl;});
-
 
     return 0;
 }
