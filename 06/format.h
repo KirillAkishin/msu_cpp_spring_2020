e668a34c7afa79b843e0590b8ad0ad5920a33a94
CyrilAk <akishin.ka@yandex.ru>
2020-05-13 13:02:09 +0300

    hw6 done

---
+++ 06/format.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <cctype>
+#include <exception>
+
+
+template<class T>
+void collect_args(std::vector<std::string>& arr, const T& arg)
+{
+	std::stringstream stream;
+	stream << arg;
+	arr.push_back(stream.str());
+}
+
+template<class T, class... ArgsT>
+void collect_args(std::vector<std::string>& arr, const T& arg, const ArgsT&... args)
+{
+	std::stringstream stream;
+	stream << arg;
+	arr.push_back(stream.str());
+
+	collect_args(arr, args...);
+}
+
+
+std::string format(const char* str)
+{
+	return str;
+}
+
+template<class... ArgsT>
+std::string format(const char* str, const ArgsT&... args)
+{
+	std::vector<std::string> arr;
+	collect_args(arr, args...);
+
+	std::stringstream stream;
+
+	for (; *str; str++)
+	{
+		if (*str != '{')
+		{
+			if (*str != '}') stream << *str;
+			else throw std::runtime_error("extra close brace");
+		}
+		else
+		{
+			str++;
+
+			if (std::isdigit(*str))
+			{
+				uint8_t index = *str - '0';
+				for (str++; std::isdigit(*str); str++)
+					index = 10*index + (*str - '0');
+
+				if (*str == '}')
+				{
+					if (index >= arr.size())
+						throw std::runtime_error("invalid argument");
+
+					stream << arr[index];
+				}
+				else throw std::runtime_error("missing close brace");
+			}
+			else throw std::runtime_error("missing argument index");
+		}
+	}
+
+	return stream.str();
+}
---
+++ 06/test.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cassert>
+#include <sstream>
+#include <vector>
+
+#include "format.h"
+
+
+#define checkEqual(x, y) \
+    do { \
+        if ((x) != (y)) \
+        { \
+            return 1; \
+        }; \
+        return 0; \
+    } while(0)
+
+#define checkThrow(x) \
+    do { \
+        try { (x); } \
+        catch (const std::runtime_error&) { return 0; } \
+        catch (...) {} \
+        return 1; \
+    } while(0)
+
+struct Test
+{
+};
+
+std::ostream& operator<<(std::ostream& out, const Test&)
+{
+    out << "test";
+    return out;
+}
+
+const Test test;
+using tcase = int (*)();
+std::vector<tcase> testCases = 
+    {
+        []() { checkEqual(format(""), ""); },
+        []() { checkEqual(format("1"), "1"); },
+        []() { checkEqual(format("{1}", 1, 2), "2"); },
+        []() { checkEqual(format("{0}{0}", "a"), "aa"); },
+        []() { checkEqual(format("{0}", Test()), "test"); },
+        []() { checkEqual(format("{0}", const_cast<Test&>(test)), "test"); },
+        []() { checkEqual(format("{0}", test), "test"); },
+        []() { checkEqual(format("{1} aaa {0}{2} {1}", 1, test, "kek"), "test aaa 1kek test"); },
+
+        []() { checkThrow(format("{", 1)); },
+        []() { checkThrow(format("{0", 1)); },
+        []() { checkThrow(format("}", 1)); },
+        []() { checkThrow(format("{1}", 1)); },
+        []() { checkThrow(format("{0}{1}{3}", 1, 2)); },
+        []() { checkThrow(format("{0{}", 1)); },
+        []() { checkThrow(format("{0a}", 1)); },
+        []() { checkThrow(format("0}", 1)); },
+        []() { checkThrow(format("{{0}", 1)); },
+     };
+
+int main(int argc, char* argv[]){
+    if (argc != 2){
+        printf("test.exe run incorrectly\n");
+        return 1;
+    }
+
+    printf("\nTesting started...\n");
+    int testCounter = 0;
+    int status;
+    for (auto& func : testCases)
+    {
+        testCounter++;
+        status = func();
+        if (status != 0)
+        {
+            printf("Test number (%i) failed with status: %i\n", testCounter, status);   
+            return 1;
+        }
+    }
+    printf("All tests passed!\n");   
+    return 0;
+}
+
+
